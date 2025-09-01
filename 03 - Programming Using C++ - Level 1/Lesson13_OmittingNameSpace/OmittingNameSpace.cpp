4c8987deb9a3884b16e78ce0c3b6e0da84774be4
nada <ns1592386@gmai.com>
2025-09-01 22:32:34 +0300

    lesson13 homework

---
+++ 03 - Programming Using C++ - Level 1/Lesson13_OmittingNameSpace/OmittingNameSpace.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	//APP1
+	cout << "****************************\n";
+	cout << "Name: Nama Salem\n";
+	cout << "Age: 23\n";
+	cout << "City: Alex.\n";
+	cout << "Country: Egypt\n";
+	cout << "****************************\n";
+	cout << "----------------------------------------------------------------------------\n";
+	//APP2
+	cout << "***********\n";
+	cout << "***********\n";
+	cout << "***********\n";
+	cout << "***********\n";
+	cout << "----------------------------------------------------------------------------\n";
+	//APP3
+	cout << "l love programming!\n\n";
+	cout << "i promise to be the best programmer ever!\n\n";
+	cout << "i know it will take some time to practice, but i will achive my goal.\n\n";
+	cout << "best regards\n";
+	cout << "nada salem\n";
+	cout << "----------------------------------------------------------------------------\n";
+	//APP4
+	cout << "*       *\n";
+	cout << "*       *\n";
+	cout << "* * * * *\n";
+	cout << "*       *\n";
+	cout << "*       *\n";
+	cout << "----------------------------------------------------------------------------\n";
+	return 0;
+}
