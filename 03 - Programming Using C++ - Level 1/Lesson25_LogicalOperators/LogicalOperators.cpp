51ac5b428ee6afd9b3f4acf336fd269d10c96bd4
nada <ns1592386@gmai.com>
2025-09-01 21:25:48 +0300

    lesson25 homework

---
+++ 03 - Programming Using C++ - Level 1/Lesson25_LogicalOperators/LogicalOperators.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << (12 >= 12) << endl; //1
+    cout << (12 > 7) << endl; //1
+    cout << (8 < 6) << endl; //0
+    cout << (8 == 8) << endl; //1
+    cout << (12 <= 12) << endl; //1
+    cout << (7 == 5) << endl; //0
+    cout << !(12 >= 12) << endl; //0
+    cout << !(12 > 7) << endl; //0
+    cout << !(8 < 6) << endl; //1
+    cout << !(8 == 8) << endl; //0
+    cout << !(12 <= 12) << endl; //0
+    cout << !(7 == 5) << endl; //1
+    cout << (1 && 1) << endl; //1
+    cout << (true && 0) << endl; //0
+    cout << (0 || 1) << endl; //1
+    cout << (0 || 0) << endl; //0
+    cout << (!0) << endl; //1
+    cout << (!(1 || 0)) << endl; //0
+    cout << (7 == 7 && 7 > 5) << endl; //1
+    cout << (7 == 7 && 7 < 5) << endl; //0
+    cout << (7 == 7 || 7 < 5) << endl; //1
+    cout << (7 == 7 || 7 > 5) << endl; //1
+    cout << (!(7 == 7) && 7 > 5) << endl; //0
+    cout << ((7 == 7) && !(7 < 5)) << endl; //1
+    cout << ((5 > 6 && 7 == 7) || (1 || 0)) << endl; //1
+    cout << (!(5 > 6 && 7 == 7) || (1 || 0)) << endl; //1
+    cout << (!(5 > 6 && 7 == 7) || !(1 || 0)) << endl; //1
+    cout << (!(5 > 6 && 7 == 7) && !(1 || 0)) << endl; //0
+    cout << (((5 > 6 && 7 <= 8) || (8 > 1 && 4 <= 3)) && true) << endl;//0
+    cout << (((5 > 6 && !(7 <= 8)) || (8 > 1 && 4 <= 3)) || true) << endl; // 1
+}
+
