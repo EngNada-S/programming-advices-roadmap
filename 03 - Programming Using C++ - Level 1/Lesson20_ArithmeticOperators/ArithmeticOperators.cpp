920d78f8b8c707d7b3119df0e1b11bdcca17bc5e
nada <ns1592386@gmai.com>
2025-09-01 20:15:16 +0300

    lesson20 homework

---
+++ 03 - Programming Using C++ - Level 1/Lesson20_ArithmeticOperators/ArithmeticOperators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short int A, B;
+    cout << "please enter the first Number A?\n";
+    cin >> A;
+    cout << "please enter the second Number B?\n";
+    cin >> B;
+    cout << endl;
+    cout << A << " + " << B << " = " << A + B << endl;
+    cout << A << " - " << B << " = " << A - B << endl;
+    cout << A << " * " << B << " = " << A * B << endl;
+    cout << A << " / " << B << " = " << A / B << endl;
+    cout << A << " % " << B << " = " << A % B << endl;
+    return 0;
+}
