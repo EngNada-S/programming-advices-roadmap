eb71342760145bd3849b04d3627798283a5782bd
nada <ns1592386@gmai.com>
2025-09-01 17:44:36 +0300

    lesson12 homework

---
+++ 03 - Programming Using C++ - Level 1/Lesson12_Output/Output.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+	//APP1
+	std::cout << "****************************\n";
+	std::cout << "Name: Nama Salem\n";
+	std::cout << "Age: 24\n";
+	std::cout << "City: Alex.\n";
+	std::cout << "Country: Egypt\n";
+	std::cout << "****************************\n";
+	std::cout << "----------------------------------------------------------------------------\n";
+	//APP2
+	std::cout << "***********\n";
+	std::cout << "***********\n";
+	std::cout << "***********\n";
+	std::cout << "***********\n";
+	std::cout << "----------------------------------------------------------------------------\n";
+	//APP3
+	std::cout << "l love programming!\n\n";
+	std::cout << "i promise to be the best programmer ever!\n\n";
+	std::cout << "i know it will take some time to practice, but i will achive my goal.\n\n";
+	std::cout << "best regards\n";
+	std::cout << "nada salem\n";
+	std::cout << "----------------------------------------------------------------------------\n";
+	//APP4
+	std::cout << "*       *\n";
+	std::cout << "*       *\n";
+	std::cout << "* * * * *\n";
+	std::cout << "*       *\n";
+	std::cout << "*       *\n";
+	std::cout << "----------------------------------------------------------------------------\n";
+	return 0;
+}
+
