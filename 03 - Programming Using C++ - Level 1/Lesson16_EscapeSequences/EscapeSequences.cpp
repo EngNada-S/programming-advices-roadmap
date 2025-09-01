0cc19840899b40660bd95b34d444ff7fd6e2a37f
nada <ns1592386@gmai.com>
2025-09-01 17:51:16 +0300

    lesson16 homework

---
+++ 03 - Programming Using C++ - Level 1/Lesson16_EscapeSequences/EscapeSequences.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	// APP1
+	cout << "\a\n";
+	//APP2
+	cout << "Dear sir\\ Modam\n\n";
+	cout << "How Are You?\n\n";
+	cout << "My Name is \"Nada\", nice to meet you.\n\n";
+	//APP3
+	cout << "Ali\tAhmed\tLira\n";
+	cout << "Fady\tZain\tMara\n";
+	return 0;
+}
