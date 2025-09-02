3ab3a1f883909f5abc39a466fa57aa1f7ab91c61
nada <ns1592386@gmai.com>
2025-09-02 21:09:10 +0300

    lesson32 homework

---
+++ 03 - Programming Using C++ - Level 1/Lesson32_FunctionP1/FunctionP1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void DisplayMyCard() {
+    cout << "****************************\n";
+    cout << "Name: Nada Salem\n";
+    cout << "Age: 24 years\n";
+    cout << "City: Alex\n";
+    cout << "Country: Egypt\n";
+    cout << "****************************\n";
+}
+
+void squareStars() {
+    cout << "***********\n";
+    cout << "***********\n";
+    cout << "***********\n";
+    cout << "***********\n";
+}
+void PrintILoveProgramming() {
+    cout << "l love programming!\n\n";
+    cout << "i promise to be the best programmer ever!\n\n";
+    cout << "i know it will take some time to practice, but i will achive my goal.\n\n";
+    cout << "best regards\n";
+    cout << "Nada Salem\n";
+}
+
+void PrintH() {
+    cout << "*       *\n";
+    cout << "*       *\n";
+    cout << "* * * * *\n";
+    cout << "*       *\n";
+    cout << "*       *\n";
+}
+int main()
+{
+    DisplayMyCard();
+    cout << "---------------------------------------\n";
+    squareStars();
+    cout << "---------------------------------------\n";
+    PrintILoveProgramming();
+    cout << "---------------------------------------\n";
+    PrintH();
+    return 0;
+}
